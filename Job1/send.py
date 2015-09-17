d64aa2e1bf1498ec80e35e072750210164aaef5c
Yuan <huangy22@gmail.com>
2015-09-17 15:17:07 +0800

    jobs

--- Job1/send.py
+++ Job1/send.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import random
-import os
+import os, sys
+IsCluster=False
 sourcedir="../program/"
 execute="XXZ"
 Dim = 3
@@ -28,7 +29,7 @@ if(os.path.exists(outfilepath)!=True):
 for eachline in inlist:
         i+=1
         para=eachline.split()
-        for j in range(1,int(para[-1])):
+        for j in range(int(para[-1])):
             seed-=1
             infile="_in"+str(i)+"_"+str(j)
             outfile="_out"+str(i)+"_"+str(j)
@@ -41,10 +42,27 @@ for eachline in inlist:
             item.append(str(seed))
             item.append(str(nblck))
             item.append(str(isload))
-            item.append("_".join(para[0:-1])+"_"+str(j)+".dat")
+            item.append("_".join(para[2:-1])+"_"+str(j)+".dat")
             stri=" ".join(item)
             f.write(str(Dim)+"\n")
             f.write("Pyrochlore "+stri)
             f.close()
-            elem=str(i)
-            os.system("./"+execute+" < "+infilepath+"/"+infile+" > "+outfilepath+"/"+outfile)
+
+            if IsCluster==False:
+                os.system("./"+execute+" < "+infilepath+"/"+infile+" > "+outfilepath+"/"+outfile)
+            else:
+                with open(jobfile, "w") as fjob:
+                    fjob.write("#!/bin/sh\n"+"#PBS -N "+jobname+"\n")
+                    if hasattr(job_atom, "pbs_command"):
+                        fjob.write(job_atom.pbs_command+"\n")
+                    fjob.write("#PBS -o "+homedir+"/Output\n")
+                    fjob.write("#PBS -e "+homedir+"/Error\n")
+                    fjob.write("echo $PBS_JOBID >>"+homedir+"/id_job.log\n")
+                    fjob.write("cd "+homedir+"\n")
+                    fjob.write(job_atom.execute+" -f "+infilepath+"/"+infile)
+
+                    os.system("qsub "+jobfile)
+                    os.system("rm "+jobfile)
+                    print("job "+jobfile+" submitted!")
+print("Jobs manage daemon is ended")
+sys.exit(0)
